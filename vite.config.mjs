f41c1d4c1267836e06e40967ee086721b7992a4e
Saipravallika2001 <saipravallika009@gmail.com>
2025-10-06 10:52:04 -0700

    commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Maguluri_Portfolio/',
   plugins: [react()],
-  base: "/Maguluri_Portfolio/", // <-- optional: helps with GitHub Pages
-});
-
+})
